4cd8a413edc4174b639a7f93d996dcec8059be10
DeepanshuChugh <deepanshuchugh24@gmail.com>
2022-05-29 15:34:22 +0530

    chakra

---
+++ materialui/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import WithSubnavigation from './components/Navbar1';
+import SimpleSidebar from './components/Sidebar';
+// import Navbar1 from './components/Navbar1';
+
+function App() {
+  return (
+    <div className="App">
+    <WithSubnavigation/>
+    <SimpleSidebar/>
+    </div>
+  );
+}
+
+export default App;
